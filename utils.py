8d0d15ab3d3986c74c901b8e98854e9d18e5e1b5
Jack McCarthy <115606026+jwmccarthy@users.noreply.github.com>
2024-07-20 19:38:58 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,5 +6,5 @@ def transpose_dict(d):
     return [dict(zip(d, v)) for v in zip(*d.values())]
 
 
-def safe_from_numpy(v):
+def try_from_numpy(v):
     return th.from_numpy(v) if isinstance(v, np.ndarray) else v
