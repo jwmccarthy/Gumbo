946ad33fef8cfd91dfb7961ec8bafa4d21ec145a
SoupChefMarco <jwmccarthy11@gmail.com>
2024-07-24 21:47:50 -0500

    new image

--- gae_speedtest.py
+++ gae_speedtest.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+from torchaudio.functional import fftconvolve
 
 import timeit
 import numpy as np
@@ -24,6 +25,20 @@ def gae_estimate_conv(rewards, values, final_value):
 
     return advantages
 
+def gae_estimate_fft_conv(rewards, values, final_value):
+    T = len(rewards)
+
+    # compute TD errors
+    next_vals = torch.cat([values[1:], final_value])
+    td_errors = rewards + gamma * next_vals - values
+
+    # conv kernel of discount factors
+    kernel = (gamma * lmbda) ** torch.arange(T, dtype=torch.float32).to(td_errors.device)
+
+    advantages = fftconvolve(td_errors, kernel.flip(0))
+
+    return advantages
+
 def gae_estimate_loop(rewards, values, final_value):
     next_vals = torch.cat([values[1:], final_value])
     td_errors = rewards + gamma * next_vals - values
@@ -37,8 +52,9 @@ def gae_estimate_loop(rewards, values, final_value):
     return advantages
 
 def test_gae_estimate():
-    ep_lens = np.arange(250, 5001, 250)
-    device = "cpu"
+    n = 10
+    ep_lens = np.array([100, 1000, 10000, 100000])
+    device = "cuda"
 
     # initialize random test values
     rewards = [torch.rand(n).to(device) for n in ep_lens]
@@ -46,20 +62,57 @@ def test_gae_estimate():
     final_value = torch.rand(1).to(device)
 
     # compute GAE estimates
-    conv_times = np.array([timeit.repeat(lambda: gae_estimate_conv(r, v, final_value), number=10) for r, v in zip(rewards, values)])
-    loop_times = np.array([timeit.repeat(lambda: gae_estimate_loop(r, v, final_value), number=10) for r, v in zip(rewards, values)])
+    conv_times = []
+    for r, v in zip(rewards, values):
+        for _ in range(10): gae_estimate_conv(r, v, final_value) # warm up
+        conv_times.append(timeit.repeat(lambda: gae_estimate_conv(r, v, final_value), number=n))
+    conv_times = np.array(conv_times)
+
+    conv_fft_g = []
+    for r, v in zip(rewards, values):
+        for _ in range(10): gae_estimate_fft_conv(r, v, final_value) # warm up
+        conv_fft_g.append(timeit.repeat(lambda: gae_estimate_fft_conv(r, v, final_value), number=n))
+    conv_fft_g = np.array(conv_fft_g)
+
+    device = "cpu"
+
+    # initialize random test values
+    rewards = [torch.rand(n).to(device) for n in ep_lens]
+    values = [torch.rand(n).to(device) for n in ep_lens]
+    final_value = torch.rand(1).to(device)
+
+    loop_times = np.array([timeit.repeat(lambda: gae_estimate_loop(r, v, final_value), number=n) for r, v in zip(rewards, values)])
+    conv_cpu_t = np.array([timeit.repeat(lambda: gae_estimate_conv(r, v, final_value), number=n) for r, v in zip(rewards, values)])
+    conv_fft_t = np.array([timeit.repeat(lambda: gae_estimate_fft_conv(r, v, final_value), number=n) for r, v in zip(rewards, values)])
 
     # store results in DataFrame
     conv_times_flattened = conv_times.ravel()
     loop_times_flattened = loop_times.ravel()
+    conv_cpu_t_flattened = conv_cpu_t.ravel()
+    conv_fft_t_flattened = conv_fft_t.ravel()
+    conv_fft_g_flattened = conv_fft_g.ravel()
 
     df = pd.DataFrame({
-        "Episode Length": np.tile(np.repeat(ep_lens, 5), 2),
-        "Time (s)": np.concatenate([conv_times_flattened, loop_times_flattened]),
-        "Method": ["conv"] * (len(ep_lens) * 5) + ["loop"] * (len(ep_lens) * 5)
+        "Episode Length": np.tile(np.repeat(ep_lens, n/2), 5),
+        "Time (s)": np.concatenate([loop_times_flattened,
+                                    conv_cpu_t_flattened,
+                                    conv_times_flattened,
+                                    conv_fft_t_flattened,
+                                    conv_fft_g_flattened]), 
+        "Method": ["Loop"] * (len(ep_lens) * int(n/2))
+                + ["conv1d (CPU)"] * (len(ep_lens) * int(n/2)) \
+                + ["conv1d (GPU)"] * (len(ep_lens) * int(n/2)) \
+                + ["fftconvolve (CPU)"] * (len(ep_lens) * int(n/2)) \
+                + ["fftconvolve (GPU)"] * (len(ep_lens) * int(n/2)) \
     })
 
-    sns.lineplot(data=df, x="Episode Length", y="Time (s)", hue="Method")
+    plt.figure(figsize=(8, 6))
+    sns.set_theme()
+    g = sns.lineplot(data=df, x="Episode Length", y="Time (s)", hue="Method")
+    g.set_xscale("log")
+    g.set_yscale("log")
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     test_gae_estimate()
